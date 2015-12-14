71c426790331ba5f456b217091c5d137c4b2c4a6
AnthonyCorrado <amcstudios@yahoo.com>
2015-12-13 16:12:16 -0800

    added house detail path

--- server/controllers/houses.js
+++ server/controllers/houses.js
@@ -1,5 +1,17 @@
-var houses = require('../housesData/allHouses');
+var fs = require('fs');
 
-exports.index = function (req, res, next) {
-  res.send({houses:houses});
-}
+exports.index = function(req, res, next) {
+};
+
+exports.show = function(req, res, next) {
+  var path = 'server/housesData/' + req.params.house_id + '.json';
+  fs.readFile(path, function(err, data) {
+    if (err) {
+      res.send({houses: err})
+      return console.log(err);
+    }
+    var obj = JSON.parse(data);
+    res.send({houses: obj})
+
+  });
+};
--- server/routes/houses.js
+++ server/routes/houses.js
@@ -1,6 +1,6 @@
 var houses = require('../controllers/houses');
 
 module.exports = function(app) {
-
   app.get('/houses', houses.index);
-}
+  app.get('/houses/:house_id', houses.show);
+};
